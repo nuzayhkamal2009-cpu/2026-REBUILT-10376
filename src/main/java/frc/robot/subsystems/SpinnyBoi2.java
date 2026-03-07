6fc61235d80bd16d27136e69e26eb3d4038e27d6
John Fogarty <john.fogarty@aol.com>
2026-03-06 22:30:32 -0800

    Added Motor

--- src/main/java/frc/robot/subsystems/SpinnyBoi2.java
+++ src/main/java/frc/robot/subsystems/SpinnyBoi2.java
@@ -3,16 +3,21 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.thethriftybot.devices.ThriftyNova;
+import com.thethriftybot.devices.ThriftyNova.CurrentType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SpinnyBoi2 extends SubsystemBase {
-    public final SparkMax m_intake = new SparkMax(5, MotorType.kBrushed);
-    public final SparkMax m_feed = new SparkMax(6, MotorType.kBrushed);
+    public final ThriftyNova  m_intake = new ThriftyNova (5);
+    public final SparkMax  m_feed = new SparkMax(6, MotorType.kBrushed);
 
     public SpinnyBoi2() {
         // SparkMaxConfig has no constructor taking an ID — use default and configure if needed
-        final SparkMaxConfig kIntakeConfigs = new SparkMaxConfig();
+        m_intake.setMaxCurrent(CurrentType.STATOR, 60)
+               .setBrakeMode(true);
+
         final SparkMaxConfig kFeedConfigs = new SparkMaxConfig();
+        kFeedConfigs.smartCurrentLimit(80);
     }
 }
