7a463a3d21f10602b2857b90d8c530fddaafb307
nuzayhkamal2009-cpu <nuzayhkamal2009@gmail.com>
2026-03-17 17:17:37 -0400

    feat: working drivetrain with nova's

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,52 @@
+package frc.robot;
+
+import com.thethriftybot.devices.ThriftyNova;
+import com.thethriftybot.devices.ThriftyNova.CurrentType;
+
+
+public final class Constants {
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+    }
+
+    public static class Drivek {
+        public static final int kLeftLeaderCANID = 3;
+        public static final int kLeftFollowerCANID = 4;
+        public static final int kRightLeaderCANID = 2;
+        public static final int kRightFollowerCANID = 1;
+        public static final int kIntakeLeaderCANID = 5;
+        public static final int kIntakeFollowerCANID = 6;
+
+    // ThriftyNova does its own internal configuration. Keep CAN IDs here.
+
+        // No-op configurator for ThriftyNova wiring placeholder. If you need
+        // specific ThriftyNova configuration (current limits, brake mode, etc.),
+        // add it here using the ThriftyNova API.
+        public static void kdriveConfigurator(ThriftyNova rightMotor, ThriftyNova leftMotor) {
+            // Apply sane defaults for drive motors: current limits and brake mode.
+            // Assumptions: ThriftyNova exposes chainable setters named
+            // setMaxCurrent(CurrentType, int) and setBrakeMode(boolean)
+            // and setInverted(boolean).
+            if (rightMotor != null) {
+                rightMotor.setMaxCurrent(CurrentType.STATOR, 60);
+                rightMotor.setBrakeMode(true);
+                rightMotor.setInverted(false);
+            }
+
+            if (leftMotor != null) {
+                leftMotor.setMaxCurrent(CurrentType.STATOR, 60);
+                leftMotor.setBrakeMode(true);
+                leftMotor.setInverted(false);
+            }
+        }
+
+        public static void kdriveConfigurator(ThriftyNova upMotor) {
+            // Feeder/intake: allow higher supply limit but lower stator limit.
+            if (upMotor != null) {
+                upMotor.setMaxCurrent(CurrentType.STATOR, 60);
+                upMotor.setBrakeMode(false);
+                upMotor.setInverted(false);
+            }
+        }
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,208 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.SpinnyBoi2;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private Drive m_drive = new Drive();
+
+  private final Timer m_autoTimer = new Timer();
+
+  private Command m_autonomousCommand;
+
+  private final RobotContainer m_robotContainer;
+  private final SpinnyBoi2 m_spinnyboi2 = new SpinnyBoi2();
+  //private final SpinnyBoi2 m_FalconShooter = new SpinnyBoi2();
+
+  private final XboxController m_driver = new XboxController(0);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    
+    CommandScheduler.getInstance().run();
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
+
+    // Start/reset the autonomous timer for the simple timed routine below
+    m_autoTimer.reset();
+    m_autoTimer.start();
+  }
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // If an autonomous command is running (from RobotContainer), prefer that.
+    if (m_autonomousCommand != null) {
+      return;
+    }
+    double time = m_autoTimer.get();
+
+    // 0–2 seconds: drive forward
+    if (time < 2.0) {
+      // TankDrive forward both sides
+      m_drive.TankDrive(0.5, 0.5);
+      m_spinnyboi2.setShooterVoltage(6);
+      m_spinnyboi2.setFeederVoltage(0);
+    }
+
+    // 2–4 seconds: spin up shooter (stop driving)
+    else if (time < 4.0) {
+      m_drive.TankDrive(0, 0);
+      m_spinnyboi2.setShooterVoltage(6.0);
+      m_spinnyboi2.setFeederVoltage(0);
+    }
+
+    // 4–20 seconds: feed game piece while shooter runs
+    else if (time < 20.0) {
+      m_drive.TankDrive(0, 0);
+      m_spinnyboi2.setShooterVoltage(6.0);
+      m_spinnyboi2.setFeederVoltage(5.0);
+    }
+
+    // stop everything after sequence
+    else {
+      m_spinnyboi2.setShooterVoltage(0);
+      m_spinnyboi2.setFeederVoltage(0);
+      m_drive.TankDrive(0, 0);
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+   m_drive.TankDrive(-(m_driver.getRightY()), (m_driver.getLeftY()));
+    
+    if (m_driver.getRawButton(5)) {
+      // LB
+          //these have a max range of (-1 to +1)
+      // m_spinnyboi2.m_feed.set(-1.0);
+      // m_spinnyboi2.m_intake_shooter.set(-1.0);
+
+      m_spinnyboi2.setShooterVoltage(9);
+      m_spinnyboi2.setFeederVoltage(6);
+
+    } else if (m_driver.getRawButton(1)) {
+      // A
+          //these have a max range of (-1 to +1)
+      // m_spinnyboi2.m_intake_shooter.set(-1.0);
+
+      m_spinnyboi2.setShooterVoltage(12);
+      m_spinnyboi2.setFeederVoltage(0);
+
+    } else if (m_driver.getRawButton(6)) {
+      // RB
+          //these have a max range of (-1 to +1)
+      // m_spinnyboi2.m_feed.set(1.0);
+      //   m_spinnyboi2.m_intake_shooter.set(-1.0);
+
+      m_spinnyboi2.setShooterVoltage(6);
+      m_spinnyboi2.setFeederVoltage(-6);
+
+    } 
+    else {
+             //these have a max range of (-1 to +1)
+      // m_spinnyboi2.m_intake_shooter.stopMotor();
+      // m_spinnyboi2.m_feed.stopMotor();
+
+      m_spinnyboi2.setShooterVoltage(0);
+      m_spinnyboi2.setFeederVoltage(0);
+
+    }
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
